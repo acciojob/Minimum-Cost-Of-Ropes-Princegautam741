888235e80d2595d6f877bf97bfb73ea24d685929
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 15:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function calculateMinCost() {
-  // Get the input rope lengths as a comma-separated string
   const ropeLengthsInput = document.getElementById('rope-lengths').value;
 
-  // Parse the input into an array of rope lengths
   const ropeLengths = ropeLengthsInput.split(',').map(length => parseInt(length));
 
-  // Check if there's only one rope
   if (ropeLengths.length < 2) {
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = 'Minimum cost of ropes: 0';
@@ -14,22 +11,16 @@ function calculateMinCost() {
 
   let totalCost = 0;
 
-  // Iterate through the sorted rope lengths and calculate the minimum cost
   while (ropeLengths.length > 1) {
-    // Sort the rope lengths in ascending order
     ropeLengths.sort((a, b) => a - b);
 
-    // Combine the two smallest ropes
     const combinedLength = ropeLengths[0] + ropeLengths[1];
 
-    // Update the total cost
     totalCost += combinedLength;
 
-    // Remove the two smallest ropes and add the combined rope
     ropeLengths.splice(0, 2, combinedLength);
   }
 
-  // Display the minimum cost in the result div
   const resultDiv = document.getElementById('result');
   resultDiv.innerHTML = `Minimum cost of ropes: ${totalCost}`;
 }
